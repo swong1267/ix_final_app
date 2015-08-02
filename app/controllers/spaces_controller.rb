89e0acc4896959ca433812f613d977156fd75760
Serena Wong <serenaw@stanford.edu>
2015-08-02 22:51:31 +0200

    finished basic controller for spaces

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -2,4 +2,46 @@ class SpacesController < ApplicationController
   def index
     @spaces = Space.all
   end
+
+  def show
+    @space = Space.find(params[:id])
+  end
+
+  def new
+    @space = Space.new
+  end
+
+  def edit
+    @space = Space.find(params[:id])
+  end
+
+  def create
+    @space = current_user.spaces.build(space_params)
+    if @space.save
+      redirect_to space_path(@space)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @space = Space.find(params[:id])
+    if @space.update(space_params)
+      redirect_to space_path(@space)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @space = Space.find(params[:id])
+    @space.destroy
+    redirect_to spaces_path
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:name, :category, :description, :location, :price, :capacity)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.delete_all
+
+ian = User.create! email: "ian@ian.com", password: "12345678", password_confirmation: "12345678"
+brian = User.create! email: "brian@brian.com", password: "12345678", password_confirmation: "12345678"
+erica = User.create! email: "erica@erica.com", password: "12345678", password_confirmation: "12345678"
+
 Space.delete_all
 
-Space.create! name: "Once", category: "gathering", description: "hipster hostel", location: "Cape Town", price: 200, capacity: 40
-Space.create! name: "iX House", category: "media room", description: "movie room with fat sacsl", location: "Cape Town", price: 100, capacity: 20
-Space.create! name: "Knead", category: "gathering", description: "delicious food", location: "Cape Town", price: 250, capacity: 50
+Space.create! name: "Once", category: "gathering", description: "hipster hostel", location: "Cape Town", price: 200, capacity: 40, user: ian
+Space.create! name: "iX House", category: "media room", description: "movie room with fat sacsl", location: "Cape Town", price: 100, capacity: 20, user: brian
+Space.create! name: "Knead", category: "gathering", description: "delicious food", location: "Cape Town", price: 250, capacity: 50, user: erica
+
