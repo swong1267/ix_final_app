5db0951792c02f043122c52afc6d6496350893e9
Serena Wong <serenaw@stanford.edu>
2015-08-02 21:29:04 +0200

    added associations in model.rb

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,2 +1,4 @@
 class Space < ActiveRecord::Base
+  has_many :requests
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    resources :requests
+    resources :spaces
+  end
+
+  resources :spaces do
+    resources :requests
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
