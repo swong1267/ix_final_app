99059348c144989b86638a1afd94ba874de0ce3f
Serena Wong <serenaw@stanford.edu>
2015-08-03 20:21:03 +0200

    added dropdown for rating and validations for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :space
+  validates :rating, :body, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
 end
