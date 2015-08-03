3bf36b3a02060c89a703c65b802e0d8b54020b74
cxmi <christinexmi@gmail.com>
2015-08-03 21:25:13 +0200

    working calendar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803152739) do
+ActiveRecord::Schema.define(version: 20150803162812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150803152739) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.integer  "space_id"
+    t.date     "date"
   end
 
   add_index "events", ["space_id"], name: "index_events_on_space_id", using: :btree
