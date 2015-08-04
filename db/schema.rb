7452215cc6fbcc20c06239041dca39257b75a0ec
Serena Wong <serenaw@stanford.edu>
2015-08-04 16:25:25 +0200

    added inquiry model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803193951) do
+ActiveRecord::Schema.define(version: 20150804141831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,17 @@ ActiveRecord::Schema.define(version: 20150803193951) do
 
   add_index "events", ["space_id"], name: "index_events_on_space_id", using: :btree
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
+
+  create_table "inquiries", force: :cascade do |t|
+    t.integer  "space_id"
+    t.integer  "event_id"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "inquiries", ["event_id"], name: "index_inquiries_on_event_id", using: :btree
+  add_index "inquiries", ["space_id"], name: "index_inquiries_on_space_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
     t.integer  "sender_id"
