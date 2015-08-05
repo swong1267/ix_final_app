d3825cfa66f14043eee08e5dc61d32720c773e2c
Serena Wong <serenaw@stanford.edu>
2015-08-05 19:40:32 +0200

    deleted remove avatar migrations

--- db/migrate/20150805082756_remove_avatar_file_size_from_events.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAvatarFileSizeFromEvents < ActiveRecord::Migration
-  def change
-    remove_column :events, :avatar_file_size, :integer
-  end
-end
--- db/migrate/20150805082830_remove_avatar_updated_at_from_spaces.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAvatarUpdatedAtFromSpaces < ActiveRecord::Migration
-  def change
-    remove_column :spaces, :avatar_updated_at, :datetime
-  end
-end
