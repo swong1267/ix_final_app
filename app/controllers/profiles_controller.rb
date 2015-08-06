d5b6728a549cbb3e28688bd531be0920298102a4
Serena Wong <serenaw@stanford.edu>
2015-08-06 14:13:06 +0200

    finished basic public profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
     @profile = Profile.find(params[:id])
     @spaces = @profile.user.spaces
     @events = @profile.user.events
+    @avg_rating = 0.0
     # @past_events = Event.where(date < Time.zone.now) && event is in @events.. @events.include? ???
   end
 end
