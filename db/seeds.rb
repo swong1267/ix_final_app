1057ea9df1902a405211d10bfca8ecc3505661ad
Serena Wong <serenaw@stanford.edu>
2015-08-07 12:47:45 +0200

    making legit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,32 @@ Space.delete_all
 Profile.delete_all
 User.delete_all
 
-John = User.create! email: "john@gmail.com", password: "12345678", password_confirmation: "12345678"
-Mary = User.create! email: "mary@gmail.com", password: "12345678", password_confirmation: "12345678"
-Sally = User.create! email: "sally@gmail.com", password: "12345678", password_confirmation: "12345678"
-Joe = User.create! email: "joe@gmail.com", password: "12345678", password_confirmation: "12345678"
 
-JohnP = Profile.create! username: "John", public: false, user: John, earnings: 20, name: "John", bio: "I run a live/work space for artists here in Cape Town, as well as another arts complex with a gallery, art studios, and dance studio. I like to have fun with life and keep positive people around me all the time."
-MaryP = Profile.create! username: "Mary", public: false, user: Mary, earnings: 15, name: "Mary", bio: "I'm a modern dance choreographer/performer/educator. I've lived in Cape Town since 1996 and have seen all of its changes. I love the rich cultural diversity of Cape Town and its creative spirit."
-SallyP = Profile.create! username: "Sally", public: false, user: Sally, earnings: 200, name: "Sally", bio: "Work at Enra in Cape Town. Graduated UCT in 2011 with a major in economics. Food Lover."
-Joe = Profile.create! username: "Joe", public: false, user: Joe, earnings: 0, name: "Joe", bio: "I've been in Cape Town for over 10 years and have loved every minute of it! When I'm not home, I'm off traveling to some amazing places all over the world. I enjoy running, yoga, scuba diving and enjoying an amazing glass of wine here, there and everywhere! :)"
+Peter = User.create! email: "peter@gmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Peter", last_name: "Smith"
+Tracy = User.create! email: "tracy@gmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Tracy", last_name: "Doe"
+Sally = User.create! email: "sally@gmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Sally", last_name: "Thompson"
+Harry = User.create! email: "harry@gmail.com", password: "12345678", password_confirmation: "12345678", first_name: "Harry", last_name: "Roberts"
+
+
+PeterP = Profile.create! first_name: "Peter", public: false, user: Peter, earnings: 20, last_name: "Peter", bio: "I run a live/work space for artists here in Cape Town, as well as another arts complex with a gallery, art studios, and dance studio. I like to have fun with life and keep positive people around me all the time."
+TracyP = Profile.create! first_name: "Tracy", public: false, user: Tracy, earnings: 15, last_name: "Tracy", bio: "I'm a modern dance choreographer/performer/educator. I've lived in Cape Town since 1996 and have seen all of its changes. I love the rich cultural diversity of Cape Town and its creative spirit."
+SallyP = Profile.create! first_name: "Sally", public: false, user: Sally, earnings: 200, last_name: "Sally", bio: "Work at Enra in Cape Town. Graduated UCT in 2011 with a major in economics. Food Lover."
+HarryP = Profile.create! first_name: "Harry", public: false, user: Harry, earnings: 0, last_name: "Harry", bio: "I've been in Cape Town for over 10 years and have loved every minute of it! When I'm not home, I'm off traveling to some amazing places all over the world. I enjoy running, yoga, scuba diving and enjoying an amazing glass of wine here, there and everywhere! :)"
+
+
+PSpace1 = Space.create! name: "Once", category: "Room", description: "Our conference is great for your meetings!", location: "73 Kloof Street, Gardens, Cape Town, South Africa", price: 200, capacity: 30, user: Peter
+PSpace2 = Space.create! name: "Once", category: "Outdoor Area", description: "Choose us for your backyard braai! Full-size fireplace + small grill + full-size kitchen.", location: "73 Kloof Street, Gardens, Cape Town, South Africa", price: 300, capacity: 40, user: Peter
+PSpace3 = Space.create! name: "International School of Cape Town", category: "Room", description: "After hours, our classrooms can be used for everything from workshops to art studio space", location: "Woodland Heights, Edinburgh Close, Wynberg 7806, Cape Town, South Africa", price: 100, capacity: 30, user: Peter
+PSpace4 = Space.create! name: "International School of Cape Town", category: "Display", description: "Our walls would be perfect for displaying local artists' work.", location: "Woodland Heights, Edinburgh Close, Wynberg 7806, Cape Town, South Africa", price: 20, capacity: 1, user: Peter
+
+TSpace1 = Space.create! name: "Mzanzi's", category: "Room", description: "We're a family-owned restaurant in the heart of Langa, and we're passionate about supporting local musicians", location: "45 Harlem Ave, Langa, Cape Town, 7455", price: 20, capacity: 20, user: Tracy
+TSpace2 = Space.create! name: "Community Center", category: "Building", description: "Flexible space that includes and ampitheater, ceramics facilities, and a gathering space.", location: "Langa, Cape Town", price: 150, capacity: 60, user: Tracy
+TSpace3 = Space.create! name: "Community Center", category: "Display", description: "We would love to provide local artists the opportunity to display their work and increase their visibility", location: "Langa, Cape Town", price: 20, capacity: 40, user: Tracy
+
+
+SEvent1 = Event.create! name: "Weekly Yoga Class", location: "Cape Town", people: 15, category: "Gathering", description: "Join me for yoga at the end of a long week to help destress", public: true, user: Sally, date: DateTime.parse("2/8/2015 17:00")
+SEvent2 = Event.create! name: "Ray's 21st Birthday!", location: "Cape Town", people: 25, category: "Gathering", description: "I'm throwing my friend Ray a surprise birthday party", public: false, user: Sally, date: DateTime.parse("10/8/2015 17:00")
+SEvent3 = Event.create! name: "Weekly Yoga Class", location: "Cape Town", people: 15, category: "Gathering", description: "Join me for yoga at the end of a long week to help destress", public: true, user: Sally, date: DateTime.parse("2/8/2015 17:00")
 
 
 ian = User.create! email: "ian@ian.com", password: "12345678", password_confirmation: "12345678"
@@ -32,12 +49,12 @@ cam = User.create! email: "cam@cam.com", password: "12345678", password_confirma
 aobo = User.create! email: "aobo@aobo.com", password: "12345678", password_confirmation: "12345678"
 tim = User.create! email: "tim@tim.com", password: "12345678", password_confirmation: "12345678"
 
-ianP = Profile.create! username: "ian", public: false, user: ian, earnings: 20, name: "ian", bio: "I'm ian"
-brianP = Profile.create! username: "brian", public: false, user: brian, earnings: 0, name: "brian", bio: "I'm brian"
-ericaP = Profile.create! username: "erica", public: false, user: erica, earnings: 100, name: "ian", bio: "I'm erica"
-aoboP = Profile.create! username: "aobo", public: false, user: aobo, earnings: 350, name: "aobo", bio: "I'm aobo"
-timP = Profile.create! username: "tim", public: false, user: cam, earnings: 10, name: "cam", bio: "I'm cam"
-camP = Profile.create! username: "cam", public: false, user: tim, earnings: 15, name: "tim", bio: "I'm tim"
+ianP = Profile.create! first_name: "ian", public: false, user: ian, earnings: 20, last_name: "ian", bio: "I'm ian"
+brianP = Profile.create! first_name: "brian", public: false, user: brian, earnings: 0, last_name: "brian", bio: "I'm brian"
+ericaP = Profile.create! first_name: "erica", public: false, user: erica, earnings: 100, last_name: "ian", bio: "I'm erica"
+aoboP = Profile.create! first_name: "aobo", public: false, user: aobo, earnings: 350, last_name: "aobo", bio: "I'm aobo"
+timP = Profile.create! first_name: "tim", public: false, user: cam, earnings: 10, last_name: "cam", bio: "I'm cam"
+camP = Profile.create! first_name: "cam", public: false, user: tim, earnings: 15, last_name: "tim", bio: "I'm tim"
 
 ian_space = Space.create! name: "Once", category: "Room", description: "hipster hostel", location: "73 Kloof Street, Gardens, Cape Town, South Africa", price: 200, capacity: 40, user: ian
 brian_space = Space.create! name: "iX House", category: "Building", description: "movie room with fat sacsl", location: "19 Park Rd, Cape Town, South Africa", price: 100, capacity: 20, user: brian
