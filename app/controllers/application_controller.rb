9f4d03e40032b7283333f131688f2cd15f97f5d6
cxmi <christinexmi@gmail.com>
2015-08-07 15:19:58 +0200

    some styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(profile)
+    dashboard_profile_path(profile)
+  end
+
   def can_change_object?(object)
     if object.user != current_user
         flash[:error] = "Insufficient permissions"
